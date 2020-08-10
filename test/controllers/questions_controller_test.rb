852f7265eed9e36014ee191ef30b8ee125ccb360
Adrien Loiseau <adrien.lois@gmail.com>
2020-08-10 16:33:37 +0200

    Ex.2

---
+++ test/controllers/questions_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class QuestionsControllerTest < ActionDispatch::IntegrationTest
+  test "should get pages" do
+    get questions_pages_url
+    assert_response :success
+  end
+
+end
